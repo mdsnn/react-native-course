a38e6f2e9f8036e048a183b87a6eca738645c87e
mwiko <mwikosinyangwe@gmail.com>
2025-05-18 11:21:25 +0200

    anothercommit

--- Greet.jsx
+++ Greet.jsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
 const Greet = () => {
   return (
     <View>
       <Text>Greet</Text>
+      <Button title='Learn More' color='green' />
     </View>
   )
 }
