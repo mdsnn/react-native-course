759cdf81bb2aa2e5bb17373c050b3434e5ccda1e
mwiko <mwikosinyangwe@gmail.com>
2025-05-18 09:35:45 +0200

    firstcommit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Greet from './Greet'
+
+const App = () => {
+  return (
+    <Greet />
+      
+    
+  )
+}
+
+export default App
